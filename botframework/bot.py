e59a7a6dcc8e6aace557073b8ca8e9389b05aa48
Tao Wei <taowei@buffalo.edu>
2024-09-28 13:38:15 -0700

    botframework

--- botframework/bot.py
+++ botframework/bot.py
@@ -4,12 +4,32 @@
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
 
+import os, uuid
+from dotenv import load_dotenv
+from gradio_client import Client
+
+load_dotenv()
+
+client = Client(os.environ.get('GRADIO_SERVER'))
+
+def gradio_chat(message, session_hash=None):
+    client.session_hash = session_hash if session_hash is not None else str(uuid.uuid4())
+    
+    result = client.predict(
+            message,
+            api_name="/chat"
+    )
+    return result
 
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+        # await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+        message = turn_context.activity.text
+        conversation_id = turn_context.activity.conversation.id
+        await turn_context.send_activity(f"{gradio_chat(message, conversation_id)}")
+        
 
     async def on_members_added_activity(
         self,
